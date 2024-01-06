93d78b47f3b9cda36f71489270a89dc2f70a3f2a
Max Caulfield <ulctrix@outlook.com>
2024-01-06 10:02:30 +0800

    Update mimo.js

--- mimo.js
+++ mimo.js
@@ -1,17 +1,11 @@
 var obj = JSON.parse($response.body);
 
 obj= {
-  "source": "ios",
-  "status": "active",
-  "interval": "yearly",
-  "billingInfo": {
-    "currency": "USD",
-    "nextBillingPrice": 59.98999999999999
-  },
   "subscriptions": [
     {
-      "source": "ios",
-      "status": "active",
+      "isActive": true,
+      "source": "web",
+      "type": "pro",
       "interval": "yearly",
       "billingInfo": {
         "currency": "USD",
@@ -24,11 +18,18 @@ obj= {
       "isActive": true
     }
   ],
+  "isActive": true,
+  "source": "web",
+  "type": "pro",
+  "interval": "yearly",
+  "billingInfo": {
+    "currency": "USD",
+    "nextBillingPrice": 59.98999999999999
+  },
   "intervalCount": 1,
   "activeUntil": "2099-10-10T08:04:21+00:00",
   "createdAt": "2019-10-03T08:04:21+00:00",
-  "clientSecret": "",
-  "isActive": true
+  "clientSecret": ""
 };
 
 $done({body: JSON.stringify(obj)});
