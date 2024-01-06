130a5bf764cb5dc89367260e25a2de77132679c5
Max Caulfield <ulctrix@outlook.com>
2024-01-06 10:36:53 +0800

    Update mimo_products.js

--- mimo_products.js
+++ mimo_products.js
@@ -8,57 +8,57 @@ obj = {
     {
       "price" : 0,
       "productType" : "streak_freeze",
-      "coinPrice" : 0
+      "coinPrice" : 5
     },
     {
       "price" : 0,
       "productType" : "custom_app_icon",
-      "coinPrice" : 0
+      "coinPrice" : 5
     },
     {
       "price" : 0,
       "productType" : "lives_refill",
-      "coinPrice" : 0
+      "coinPrice" : 5
     },
     {
       "price" : 0,
       "productType" : "streak_repair",
-      "coinPrice" : 0
+      "coinPrice" : 5
     },
     {
       "price" : 0,
       "productType" : "league_freeze",
-      "coinPrice" : 0
+      "coinPrice" : 5
     },
     {
       "price" : 0,
       "productType" : "app_icon_batman",
-      "coinPrice" : 0
+      "coinPrice" : 5
     },
     {
       "price" : 0,
       "productType" : "app_icon_ninja",
-      "coinPrice" : 0
+      "coinPrice" : 5
     },
     {
       "price" : 0,
       "productType" : "app_icon_pride",
-      "coinPrice" : 0
+      "coinPrice" : 5
     },
     {
       "price" : 0,
       "productType" : "app_icon_breaking_bad",
-      "coinPrice" : 0
+      "coinPrice" : 5
     },
     {
       "price" : 0,
       "productType" : "double_sparks_24h",
-      "coinPrice" : 0
+      "coinPrice" : 5
     },
     {
       "price" : 0,
       "productType" : "streak_challenge_7d",
-      "coinPrice" : 0
+      "coinPrice" : 5
     }
   ]
 };
